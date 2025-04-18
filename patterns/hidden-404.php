35d61ac4ddcdc5dba38470ce5ecba1281b7b6984
Jorys Paulin <jorys@jorys-paulin.fr>
2025-04-18 23:12:34 +0200

    feat: add 404 template

---
+++ patterns/hidden-404.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Title: 404
+ * Slug: paperback/hidden-404
+ * Inserter: no
+ *
+ * @package Paperback
+ * @since Paperback 1.0
+ */
+
+?>
+
+<!-- wp:group {"layout":{"type":"default"}} -->
+<div class="wp-block-group">
+  <!-- wp:heading {"level":1} -->
+  <h1 class="wp-block-heading"><?php echo esc_html_x( 'Page not found', '404 error message', 'paperback' ); ?></h1>
+  <!-- /wp:heading -->
+  <!-- wp:paragraph -->
+  <p><?php echo esc_html_x( 'The page you are looking for doesn\'t exist, or it has been moved. Please try searching using the form below.', '404 error message', 'paperback' ); ?></p>
+  <!-- /wp:paragraph -->
+</div>
+<!-- /wp:group -->
