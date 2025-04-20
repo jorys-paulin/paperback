86f997c9e625807c1f7e646b6eeb84ddf0ba23f0
Jorys Paulin <jorys@jorys-paulin.fr>
2025-04-20 14:02:38 +0200

    feat: update query loop

--- patterns/template-query-loop.php
+++ patterns/template-query-loop.php
@@ -17,10 +17,10 @@
 	<!-- wp:post-template {"align":"full","layout":{"type":"default"}} -->
 		<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|60"}}},"layout":{"type":"constrained"}} -->
 		<div class="wp-block-group alignfull" style="padding-top:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60)">
-			<!-- wp:post-featured-image {"isLink":true,"aspectRatio":"3/2"} /-->
+			<!-- wp:post-featured-image {"aspectRatio":"16/9"} /-->
 			<!-- wp:post-title {"isLink":true,"fontSize":"x-large"} /-->
 			<!-- wp:post-content {"align":"full","fontSize":"medium","layout":{"type":"constrained"}} /-->
-			<!-- wp:post-date {"isLink":true,"style":{"spacing":{"margin":{"top":"var:preset|spacing|40"}}},"fontSize":"small"} /-->
+			<!-- wp:post-date {"style":{"spacing":{"margin":{"top":"var:preset|spacing|40"}}},"fontSize":"small"} /-->
 		</div>
 		<!-- /wp:group -->
 	<!-- /wp:post-template -->
