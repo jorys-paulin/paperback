334101615e72cb76afaade05b3a260b880f5559a
Jorys Paulin <jorys@jorys-paulin.fr>
2025-05-02 17:14:05 +0200

    feat: change query loop

---
+++ patterns/hidden-no-results.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Title: No results
+ * Slug: paperback/hidden-no-results
+ * Inserter: no
+ *
+ * @package Paperback
+ * @since Paperback 1.0
+ */
+
+?>
+
+<!-- wp:paragraph -->
+<p><?php echo esc_html_x( 'Sorry, but nothing was found. Please try a search with different keywords.', 'Message explaining that there are no results returned from a search.', 'paperback' ); ?></p>
+<!-- /wp:paragraph -->
--- patterns/template-query-loop.php
+++ patterns/template-query-loop.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * Title: List of posts, 1 column
+ * Title: List of posts, 3 columns
  * Slug: paperback/template-query-loop
  * Categories: query
  * Block Types: core/query
- * Description: A list of posts, 1 column, with featured image and post date.
+ * Description: A list of posts, 3 columns, with featured image and post date.
  *
  * @package Paperback
  * @since Paperback 1.0
@@ -12,35 +12,23 @@
 
 ?>
 
-<!-- wp:query {"query":{"perPage":3,"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":true,"taxQuery":null,"parents":[]},"align":"full","layout":{"type":"default"}} -->
-<div class="wp-block-query alignfull">
-	<!-- wp:post-template {"align":"full","layout":{"type":"default"}} -->
-		<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|60"}}},"layout":{"type":"constrained"}} -->
-		<div class="wp-block-group alignfull" style="padding-top:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60)">
-			<!-- wp:post-featured-image {"aspectRatio":"16/9"} /-->
-			<!-- wp:post-title {"isLink":true,"fontSize":"x-large"} /-->
-			<!-- wp:post-content {"align":"full","fontSize":"medium","layout":{"type":"constrained"}} /-->
-			<!-- wp:post-date {"style":{"spacing":{"margin":{"top":"var:preset|spacing|40"}}},"fontSize":"small"} /-->
-		</div>
-		<!-- /wp:group -->
+<!-- wp:query {"query":{"perPage":10,"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":true,"taxQuery":null,"parents":[]},"align":"wide","layout":{"type":"default"}} -->
+<div class="wp-block-query alignwide">
+	<!-- wp:post-template {"align":"full","layout":{"type":"grid","columnCount":3}} -->
+		<!-- wp:post-featured-image {"aspectRatio":"16/9"} /-->
+		<!-- wp:post-title {"isLink":true} /-->
+		<!-- wp:post-excerpt /-->
+		<!-- wp:post-date {"style":{"spacing":{"margin":{"top":"var:preset|spacing|50"}}},"fontSize":"small"} /-->
 	<!-- /wp:post-template -->
-	<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|60"}}},"layout":{"type":"constrained"}} -->
-	<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60)">
-		<!-- wp:query-no-results -->
-      <!-- wp:paragraph -->
-      <p><?php echo esc_html_x( 'Sorry, but nothing was found. Please try a search with different keywords.', 'Message explaining that there are no results returned from a search.', 'paperback' ); ?></p>
-      <!-- /wp:paragraph -->
-		<!-- /wp:query-no-results -->
-	</div>
-	<!-- /wp:group -->
-	<!-- wp:group {"align":"wide","layout":{"type":"constrained"}} -->
-	<div class="wp-block-group alignwide">
-		<!-- wp:query-pagination {"paginationArrow":"arrow","align":"wide","layout":{"type":"flex","justifyContent":"space-between"}} -->
-			<!-- wp:query-pagination-previous /-->
-			<!-- wp:query-pagination-numbers /-->
-			<!-- wp:query-pagination-next /-->
-		<!-- /wp:query-pagination -->
-	</div>
-	<!-- /wp:group -->
+	
+	<!-- wp:query-no-results -->
+		<!-- wp:pattern {"slug":"paperback/hidden-no-results"} /-->
+	<!-- /wp:query-no-results -->
+	
+	<!-- wp:query-pagination {"paginationArrow":"arrow","align":"wide","layout":{"type":"flex","justifyContent":"space-between"}} -->
+		<!-- wp:query-pagination-previous /-->
+		<!-- wp:query-pagination-numbers /-->
+		<!-- wp:query-pagination-next /-->
+	<!-- /wp:query-pagination -->
 </div>
 <!-- /wp:query -->
