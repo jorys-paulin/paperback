3353af8555f0506632fd0f5eab66771340f89d09
Jorys Paulin <jorys@jorys-paulin.fr>
2025-05-02 17:14:29 +0200

    feat: add heading to home template

---
+++ patterns/hidden-blog-heading.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Title: Hidden blog heading
+ * Slug: paperback/hidden-blog-heading
+ * Description: Hidden heading for the home page and index template.
+ * Inserter: no
+ *
+ * @package Paperback
+ * @since Paperback 1.0
+ */
+
+?>
+
+<!-- wp:heading {"level":1,"align":"wide"} -->
+<h1 class="wp-block-heading alignwide"><?php esc_html_e( 'Latest posts', 'paperback' ); ?></h1>
+<!-- /wp:heading -->
